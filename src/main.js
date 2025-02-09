17cf78d54421b82dccd72bc1e639b9e7791e826d
Malizma333 <malizma333@gmail.com>
2025-02-09 17:49:53 -0600

    settings drawer

--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
-console.log("test")
+function main() {
+  setupSettingsDrawer();
+}
+
+function setupSettingsDrawer() {
+  const drawer = document.querySelector('.drawer-placement-top');
+  const openButton = document.querySelector('sl-icon-button[name="gear"]');
+  const closeButton = drawer.querySelector('sl-button[variant="primary"]');
+
+  openButton.addEventListener('click', () => drawer.show());
+  closeButton.addEventListener('click', () => drawer.hide());
+}
+
+main();
