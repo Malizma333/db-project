9fd781977ed0076985540df7d5a891630a31086a
Malizma333 <malizma333@gmail.com>
2025-02-09 17:53:38 -0600

    shuffle dialog

--- src/main.js
+++ src/main.js
@@ -1,14 +1,20 @@
 function main() {
   setupSettingsDrawer();
+  setupRandomRecipeDialog();
 }
 
 function setupSettingsDrawer() {
   const drawer = document.querySelector('.drawer-placement-top');
   const openButton = document.querySelector('sl-icon-button[name="gear"]');
-  const closeButton = drawer.querySelector('sl-button[variant="primary"]');
 
   openButton.addEventListener('click', () => drawer.show());
-  closeButton.addEventListener('click', () => drawer.hide());
+}
+
+function setupRandomRecipeDialog() {
+  const dialog = document.querySelector('.dialog-overview');
+  const openButton = document.querySelector('sl-icon-button[name="shuffle"]');
+
+  openButton.addEventListener('click', () => dialog.show());
 }
 
 main();
