bf3cf240570f62d9295aedca54ce8bf586377ac5
Robert Boerwinkle <rfboerwinkle@gmail.com>
2025-04-06 20:24:08 -0500

    mostly format stuff. changed do_stuff interface

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,6 @@
 from http import server
 import json
 import os
-import queue
-import random
 import string
 
 FILE_ROOT = os.path.join("..", "frontend", "dist")
@@ -10,6 +8,7 @@ FILE_ROOT = os.path.join("..", "frontend", "dist")
 EXTENSIONS = {
     "html": "text/html",
     "ico": "image/x-icon",
+    "png": "image/png",
     "js": "text/javascript",
     "css": "text/css",
     "json": "application/json",
@@ -20,29 +19,18 @@ for root,dirs,files in os.walk(FILE_ROOT):
     for f_name in files:
         full_name = os.path.join(root, f_name)
         extension = f_name.split(".")[-1]
+        internal = full_name[len(FILE_ROOT):].replace("\\", "/")
         with open(full_name, "rb") as f:
-            print(full_name)
-            print(full_name[len(FILE_ROOT):])
-            SERVABLE[full_name[len(FILE_ROOT):].replace("\\", "/")] = (f.read(), EXTENSIONS[extension])
-
-# SERVABLE = {
-#     "index.html": ["index.html", "text/html"],
-#     "assets/favicon-D8EyK4Iu.ico": ["assets/favicon-D8EyK4Iu.ico", "image/x-icon"],
-#     "assets/index-COw7SYG4.js": ["assets/index-COw7SYG4.js", "text/javascript"],
-#     "assets/index-D_6z8VR5.css": ["assets/index-D_6z8VR5.css", "text/css"],
-#     "assets/manifest-D2PoeSCd.json": ["assets/manifest-D2PoeSCd.json", "application/json"],
-#     "404.html": ["404.html", "text.html"],
-# }
-# for f_name in SERVABLE:
-#     with open(f_name, "rb") as f:
-#         SERVABLE[f_name][0] = f.read()
+            SERVABLE[internal] = (f.read(), EXTENSIONS[extension])
 
 TOKEN_LENGTH = 30
 TOKEN_CHARS = string.ascii_letters + string.digits
 
+# This is where most db calls get handled. takes in parsed json. outputs object
+# ready to be run through `dumps`
 def do_thing(body):
     print(f"POST request with body {body}")
-    return "{\"type\":\"response\",\"greeting\":\"heyyyyyy\"}".encode()
+    return {"type":"response","greeting":"heyyyyyy"}
 
 class RequestHandler(server.BaseHTTPRequestHandler):
     def do_GET(self):
@@ -70,11 +58,11 @@ class RequestHandler(server.BaseHTTPRequestHandler):
         length = int(self.headers["content-length"])
         body = self.rfile.read(length)
         if p.startswith("/api"):
-            ret = do_thing(body.decode())
+            ret = do_thing(json.parse(body.decode()))
             self.send_response(200)
             self.send_header("content-type", "application/json")
             self.end_headers()
-            self.wfile.write(ret)
+            self.wfile.write(json.dumps(ret).encode())
             return 200
 
 ser = server.ThreadingHTTPServer(("",8008), RequestHandler)
