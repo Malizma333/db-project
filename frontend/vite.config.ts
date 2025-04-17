3661445619d9bfcd582f4a642360e6dde2140c90
Malizma333 <malizma333@gmail.com>
2025-04-16 21:54:02 -0500

    config stuff

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import preact from '@preact/preset-vite'
 
 export default defineConfig({
-  plugins: [preact()],
-  server: { port: 8008 }
+  plugins: [preact(), tsconfigPaths()],
+  server: { port: 8008 },
 })
