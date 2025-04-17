533a6f13df883861e033a588b7ab2c8dbf74690e
Robert Boerwinkle <rfboerwinkle@gmail.com>
2025-04-16 23:26:57 -0500

    test 404 page (untested)

--- frontend/src/components/pages/404.tsx
+++ frontend/src/components/pages/404.tsx
@@ -1,5 +1,43 @@
+const styles: Record<string, React.CSSProperties> = {
+  root: {
+    display: "flex",
+    flexDirection: "column",
+    height: "95vh",
+  },
+  banner: {
+    alignItems: "center",
+    color: "gray",
+    display: "flex",
+    flexDirection: "column",
+    flex: "11",
+    fontSize: "72px",
+    justifyContent: "center",
+    userSelect: "none",
+  }
+  art: {
+    alignItems: "center",
+    color: "gray",
+    display: "flex",
+    flexDirection: "column",
+    flex: "11",
+    fontSize: "50px",
+    justifyContent: "center",
+    userSelect: "none",
+    font-family: "monospace",
+    white-space: "pre-wrap",
+  }
+}
+
 export default function FourOFourPage() {
-  return <div>
-    Hello 404!
-  </div>
+  return (
+    <div style={styles.root}>
+      <div style={styles.banner}>
+        Hello 404!
+      </div>
+      <div style={styles.art}>
+These should line up  v
+    some more s       ^
+      </div>
+    </div>
+  );
 }
